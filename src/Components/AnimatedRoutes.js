0feef5824c621865461b2ffa02d447a0a9206c0f
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2023-11-15 11:52:45 +0000

    First commit new refactoring dev branch

--- src/Components/AnimatedRoutes.js
+++ src/Components/AnimatedRoutes.js
@@ -10,29 +10,35 @@ import Ingaggio from "../Pages/Ingaggio";
 import MediaOverall from "../Pages/MediaOverall";
 import OfferteMercato from "../Pages/OfferteMercato";
 import SerieNegativa from "../Pages/SerieNegativa";
-import BonusAnnuali from '../Pages/BonusAnnuali'
+import BonusAnnuali from "../Pages/BonusAnnuali";
+import Dashboard from "../Pages/Auth/Dashboard";
+import Login from "../Pages/Auth/Login";
+import Signup from "../Pages/Auth/Signup";
 import ErrorPage from "../Pages/ErrorPage";
 
 const AnimatedRoutes = () => {
-    const location = useLocation();
-    return (
-        <AnimatePresence>
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/prepartita" element={<Prepartita />} />
-                <Route path="/settimana" element={<Settimana />} />
-                <Route path="/regolamento" element={<Regolamento />} />
-                <Route path="/calcolo-media" element={<MediaOverall />} />
-                <Route path="/ingaggio" element={<Ingaggio />} />
-                <Route path="/rinnovi" element={<Rinnovi />} />
-                <Route path="/serie-negativa" element={<SerieNegativa />} />
-                <Route path="/offerte-mercato" element={<OfferteMercato />} />
-                <Route path="/bonus-annuali" element={<BonusAnnuali />} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </AnimatePresence>
-    );
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/prepartita" element={<Prepartita />} />
+        <Route path="/settimana" element={<Settimana />} />
+        <Route path="/regolamento" element={<Regolamento />} />
+        <Route path="/calcolo-media" element={<MediaOverall />} />
+        <Route path="/ingaggio" element={<Ingaggio />} />
+        <Route path="/rinnovi" element={<Rinnovi />} />
+        <Route path="/serie-negativa" element={<SerieNegativa />} />
+        <Route path="/offerte-mercato" element={<OfferteMercato />} />
+        <Route path="/bonus-annuali" element={<BonusAnnuali />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
 };
 
 export default AnimatedRoutes;
