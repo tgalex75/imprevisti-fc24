121cba872dedfce2fd0d2f486f1f9468cee3bbd2
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2024-02-29 13:57:03 +0000

    Mattinata buttata

--- src/Pages/EditorImprevisti.js
+++ src/Pages/EditorImprevisti.js
@@ -1,19 +1,32 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import { supabase } from "../supabaseClient";
 import { motion } from "framer-motion";
 import { MdSend, MdClear } from "react-icons/md";
+//import { useForm } from "react-hook-form";
+import useDeepCompareEffect from "use-deep-compare-effect";
 
 const EditorImprevisti = () => {
   const [vociRegistro, setVociRegistro] = useState([]);
+  const [selectRefState, setSelectRefState] = useState(null);
+
+  /* const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm(); */
 
   const aggiornaTitoloImprRef = useRef([]);
   const aggiornaDescImprRef = useRef([]);
   const nuovoImprTitoloRef = useRef(null);
   const nuovoImprDescrRef = useRef(null);
+  const selectRef = useRef(null);
 
   const handleNewImpr = (e) => {
     e.preventDefault();
     uploadNewImpr(nuovoImprTitoloRef, nuovoImprDescrRef);
+    nuovoImprTitoloRef.current.value = null;
+    nuovoImprDescrRef.current.value = null;
   };
 
   const uploadNewImpr = async (titolo, descr) => {
@@ -26,7 +39,7 @@ const EditorImprevisti = () => {
     console.log(data ? data : console.log(error));
   };
 
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     fetchRegistryList();
   }, [vociRegistro]);
 
@@ -52,6 +65,11 @@ const EditorImprevisti = () => {
     console.log(data ? data : error);
   };
 
+  const handleSelectRef = () => {
+    setSelectRefState(selectRef.current.value);
+    console.log(selectRef.current.value);
+  };
+
   return (
     <section className="flex h-full w-full flex-col items-center justify-center gap-6 px-4 pb-6 font-bold">
       <h1>Editor Imprevisti</h1>
@@ -63,10 +81,20 @@ const EditorImprevisti = () => {
       >
         {/* Lista Imprevisti Attuale */}
 
-        <div className="relative flex h-3/5 w-full flex-col gap-2">
+        <div className="relative flex h-3/5 w-full flex-col items-center gap-2">
           <h3 className="text-center uppercase text-[--clr-ter]">
             Imprevisti della Community
           </h3>
+          <select
+            ref={selectRef}
+            onChange={handleSelectRef}
+            className="w-48 rounded-md border py-1 text-sm font-semibold dark:border-black/20 dark:bg-black/30 dark:text-gray-300 dark:placeholder-black/10 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          >
+            <option selected className="bg-black/20" value="Prepartita">
+              Prepartita
+            </option>
+            <option value="Speciali">Speciali</option>
+          </select>
           <strong className="absolute right-1 top-0 font-semibold">
             # {vociRegistro.length}
           </strong>
@@ -77,14 +105,14 @@ const EditorImprevisti = () => {
                 className="text-md flex items-center justify-between gap-2 bg-gray-700/20 ps-2 text-left font-normal hover:bg-gray-600/50"
               >
                 <input
-                  className="w-1/6 rounded border border-gray-300/20 bg-transparent p-1 pe-6 font-semibold placeholder:italic placeholder:font-normal"
+                  className="w-1/6 rounded border border-gray-300/20 bg-transparent p-1 pe-6 font-semibold placeholder:font-normal placeholder:italic"
                   placeholder={el.titolo}
                   ref={(element) =>
                     (aggiornaTitoloImprRef.current[el.id] = element)
                   }
                 />
                 <input
-                  className="w-5/6 rounded border border-gray-300/20 bg-transparent p-1 pe-6 font-semibold placeholder:italic placeholder:font-normal"
+                  className="w-5/6 rounded border border-gray-300/20 bg-transparent p-1 pe-6 font-semibold placeholder:font-normal placeholder:italic"
                   placeholder={el.descrizione}
                   ref={(element) =>
                     (aggiornaDescImprRef.current[el.id] = element)
@@ -117,19 +145,34 @@ const EditorImprevisti = () => {
           <h3 className="text-center uppercase text-[--clr-ter]">
             Aggiungi il tuo imprevisto
           </h3>
-          <div className="flex h-full w-1/2 flex-col items-center justify-center gap-1 rounded-md border-4 p-8 font-normal">
-            <label className="my-1 block self-start text-xs">
+          <form
+            //onSubmit={handleSubmit((data) => console.log(data))}
+            className="flex h-full w-1/2 flex-col items-center justify-center gap-1 rounded-md border-4 p-8 font-normal"
+          >
+            <label className="my-1 flex w-full items-center gap-4 self-start text-xs">
               Titolo Imprevisto
+              {/* {errors.titolo && (
+                <small className="right-0 text-[--clr-ter]">
+                  Il campo Titolo è obbligatorio
+                </small>
+              )} */}
             </label>
             <input
+              //{...register("titolo", { required: true, maxLength: 15 })}
               className="block w-full rounded p-1 text-sm  text-black placeholder:italic"
               placeholder="Titolo dell'imprevisto"
               ref={nuovoImprTitoloRef}
             />
-            <label className="my-1 block self-start text-xs">
+            <label className="my-1 flex w-full items-center gap-4 self-start text-xs">
               Dettagli Imprevisto
+              {/* {errors.dettagli && (
+                <small className="text-[--clr-ter]">
+                  Il campo Dettagli è obbligatorio
+                </small>
+              )} */}
             </label>
             <textarea
+              //{...register("dettagli", { required: true })}
               ref={nuovoImprDescrRef}
               rows={3}
               id="nuovoImprevistoInput"
@@ -142,7 +185,7 @@ const EditorImprevisti = () => {
             >
               Salva
             </button>
-          </div>
+          </form>
         </div>
       </motion.div>
     </section>
@@ -150,3 +193,5 @@ const EditorImprevisti = () => {
 };
 
 export default EditorImprevisti;
+
+/* https://github.com/kentcdodds/use-deep-compare-effect */
